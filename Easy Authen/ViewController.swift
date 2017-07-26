5ff583d1c690eed221d5311cb07175aa2e168ec2
Com422Mac03 <com422mac03@Com422Mac03s-iMac.local>
2017-07-26 16:54:40 +0700

    Easy Authen

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -9,6 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //Imlicit
+    let strDicUser = ["master": "1234", "dodo": "11111"]
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var massageLabel: UILabel!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        //Get Value From TextField
+        let strUser: String = userTextField.text!
+        let strPassword: String = passwordTextField.text!
+        
+        print("User ==> \(strUser)")
+        print("Password ==> \(strPassword)")
+        
+        //Check User
+        if let strMyPassword = strDicUser[strUser]{
+            print("User OK")
+        }   else {
+            print("User False")
+        }
+        
+        
+    } // loginButton
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
